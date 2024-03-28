a5d97f6eddc014f16a951bd152becbda1920ad0e
Yonge2 <dlwodyd25@naver.com>
2024-03-28 21:28:09 +0900

    fix: util 모듈 import

--- src/stastics/stastics.module.ts
+++ src/stastics/stastics.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common'
 import { StasticsService } from './stastics.service'
 import { StasticsController } from './stastics.controller'
 import { StasticsRepository } from './stastics.repository'
+import { UtilDayjsService } from 'src/utils/utils.dayjs.service'
+import { UtilsModule } from 'src/utils/utils.module'
 
 @Module({
+  imports: [UtilsModule],
   controllers: [StasticsController],
-  providers: [StasticsService, StasticsRepository],
+  providers: [StasticsService, StasticsRepository, UtilDayjsService],
 })
 export class StasticsModule {}
